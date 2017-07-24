a8528e40054bde0ecec8128c295f29e1e160f3b4
Yashasvi Sinha <yashasvi.kumar.sinha@gmail.com>
2017-07-24 22:11:32 +0530

    To do new commit

--- src/app/app.js
+++ src/app/app.js
@@ -1,3 +1,25 @@
-(function(){
-    $("#my-app").html("App.js works as well !");
-})();
+
+$("#register").click(function (e) { 
+    e.preventDefault();
+    var reqPage = $(this).attr('href');
+    console.log(reqPage);
+    loadPage(reqPage);
+});
+
+function loadPage(pageName) {
+    console.log(pageName);
+    $.ajax({
+        type: "GET",
+        url: "src/pages/"+ pageName,
+        dataType: "text",
+        success: function (returnedPage) {
+            $('.container').html(returnedPage);
+        },
+        error: function (error){
+            console.log("ERROR", error);
+        },
+        complete: function(xhr,status){
+            console.log(xhr, status);
+        }
+    });
+}
