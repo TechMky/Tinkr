e95f52167c743f473497a0c393139354049f98de
SidKr <siddhartha.kumar@live.com>
2017-07-24 20:42:34 -0400

    Code Refactor. - Added comments - JQ selector refactor - Minor logic changes.

--- src/app/login.js
+++ src/app/login.js
@@ -1,9 +1,14 @@
 var loginView = (function ($) {
 
     //Declare private vars and functions here
+    var validateLogin = function(){
+        //Do stuff
+        //Private function only availble to loginView
+        console.log("validate stuff and do something else");
+    }
 
-
-    $("#app").on("click", "#signInBtn", function (e) {
+    //Event handlers
+    $("#app").on("click", "#signInLink", function (e) {
        loginView.init();
     });
 
@@ -14,6 +19,10 @@ var loginView = (function ($) {
             templateName: $(this).attr('data-page'),
             selector: "#app"
         });
+    });
+
+    $("#app").on("click", "#signInBtn", function (e) {
+       validateLogin();
     });
 
     //Reveal public functions below
